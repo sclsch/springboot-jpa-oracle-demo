4225cf94c4e053d16ab9716e5044d90a452623f7
suncl <258268936@qq.com>
2018-10-16 10:52:39 +0800

    test it again ok

--- src/main/java/com/example/demo/web/SS.java
+++ src/main/java/com/example/demo/web/SS.java
@@ -3,5 +3,7 @@ package com.example.demo.web;
 public class SS {
     public static void main(String[] args){
       System.out.println("test it again");
+      System.out.println("test it ok");
+
     }
 }
